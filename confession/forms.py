f6a9d4b933bfc0b6860b4fd6370864f3fe3e9564
Nico <remullanico28@gmail.com>
2023-10-08 23:48:02 +0800

    comments, confess form vanishing in about fix

--- confession/forms.py
+++ confession/forms.py
@@ -3,5 +3,11 @@ from . models import Confession
 
 class ConfessionForm(forms.ModelForm):
     class Meta:
+        '''
+            in the Meta class, here we specify what database table that this
+            "form" will act upon. In this case, it will perform CREATE
+            operations for Confession (table)
+            
+        '''
         model = Confession
         fields = ['content']
--- confession/views.py
+++ confession/views.py
@@ -10,16 +10,18 @@ def home(request):
 
     if request.method == 'POST':
         form = ConfessionForm(request.POST)
-        if form.is_valid():
+        if form.is_valid(): # check if there are no missing requirments from the form
+
+
             form.save()
 
-            return redirect('home')
+            return redirect('home') 
     else:
         form = ConfessionForm()
 
     context = {
         # 'confessions': Confession.objects.filter(moderated=True)
-        'confessions': Confession.objects.all(),
+        'confessions': Confession.objects.all(), # returns a QuerySet an array like data structure
         'form': form
 
     }
@@ -28,32 +30,51 @@ def home(request):
     return render(request, "confession/attempt.html", context)
 
 
-def confess(request):
-    if request.method == 'POST':
-        form = ConfessionForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-            return redirect('home')
-    else:
-        form = ConfessionForm()
-     
-
-
-
-    return render(request, "confession/confession.html", {'form': form})
-
 
 
 
 
 def about(request):
 
+    form = ConfessionForm()
+
     context = {
         # 'confessions': Confession.objects.filter(moderated=True)
-        'confessions': Confession.objects.all()
+        'confessions': Confession.objects.all(),
+        'form': form
 
     }
 
     return render(request, "confession/about.html", context)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def confess(request):
+    if request.method == 'POST':
+        form = ConfessionForm(request.POST)
+        if form.is_valid():
+
+            form.save()
+
+            return redirect('home')
+    else:
+        form = ConfessionForm()
+
+
+    return render(request, "confession/confession.html", {'form': form})
